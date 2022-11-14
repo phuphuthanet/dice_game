ef74961872537d6009757b9da5ee2a074266e69d
phuphu <62703704+phuphuthanet@users.noreply.github.com>
2022-11-14 20:21:14 +0700

    Dice Game  V1.1
    1.index.html

--- index.js
+++ index.js
@@ -8,8 +8,8 @@
   console.log(random2);
 
 
-  document.querySelector(".dice1").setAttribute("src", "images/dice"+random1+".png");
-  document.querySelector(".dice2").setAttribute("src", "images/dice"+random2+".png");
+  document.querySelector(".dice1").setAttribute("src", "dice"+random1+".png");
+  document.querySelector(".dice2").setAttribute("src", "dice"+random2+".png");
 
   if (random1 > random2) document.querySelector("h1").innerText = "Player 1 Win!";
   else if (random1 === random2) document.querySelector("h1").innerText = "Tie";
